10ef375b60f60325d4ce932c45607e6e2443f64b
As-ajay06 <as2673447@gmail.com>
2025-06-30 20:49:11 +0530

    minor update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import SearchBar from './components/SearchBar'
+import FacultyTable from './table/FacultyTable'
+import ResearchPaperTable from './table/ResearchPaperTable'
+import AwardTable from './table/AwardsTable'
+import ConferenceTable from './table/ConferenceTable'
+import StudentCertificatesTable from './table/StudentTable'
+import StudentTable from './table/StudentTable'
+import SportsTable from './table/SportsTable'
 
 
 
