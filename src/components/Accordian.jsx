cf94a8138163ca655b82636f0a2125b3e9036890
As-ajay06 <as2673447@gmail.com>
2025-07-01 13:53:45 +0530

    Added accordian component

---
+++ src/components/Accordian.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import Button from './Button';
+
+const Accordion = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  console.log(isOpen)
+
+  return (
+    <div className='w-28 border rounded-xl mb-4 shadow-md'>
+        <button 
+        className="w-28 flex justify-between items-center px-4 py-3 font-semibold bg-gray-100 hover:bg-gray-200 rounded-t-xl"
+        onClick={() => setIsOpen((p) => !p)}>Click me</button>
+    <div>
+        {isOpen && 
+        <div className='px-4 py-3 text-gray-700 bg-white rounded-b-xl border-t'>
+            <button>Students</button>
+        </div>
+        }
+    </div>
+    </div>
+  )
+}
+
+export default Accordion
